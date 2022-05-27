334312cd0bcdcd0a1ab05a492fc87dc0b5880d5e
Barros <mdbf42@gmail.com>
2022-05-27 00:01:18 -0300

    Git Ignore

--- src/lib/GoogleAnalytics.ts
+++ src/lib/GoogleAnalytics.ts
@@ -1,6 +1,7 @@
 import Settings from '../core/settings';
 
 export const pageview = (url: string) => {
+  // @ts-ignore
   window.gtag('config', Settings.getGoogleTagKey(), {
     page_path: url,
   });
@@ -8,6 +9,7 @@ export const pageview = (url: string) => {
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }: any) => {
+  // @ts-ignore
   window.gtag('event', action, {
     event_category: category,
     event_label: label,
