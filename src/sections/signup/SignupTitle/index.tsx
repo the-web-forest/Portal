1f870e4e7319c360295946bd92a5be8592eeb97f
Barros <mdbf42@gmail.com>
2022-08-10 14:58:44 -0300

    Feature @ Remove Footer

--- src/sections/signup/SignupTitle/index.tsx
+++ src/sections/signup/SignupTitle/index.tsx
@@ -5,7 +5,7 @@ export const SignupTitle: FC = () => (
   <div className={styles.container}>
     <h1 className={styles.title}>Cadastre-se</h1>
     <span className={styles.description}>
-      Através do cadastro você apoia nossa causa e planta mais arvores
+      Através do cadastro você apóis nossa causa e planta mais árvores.
     </span>
   </div>
 );
