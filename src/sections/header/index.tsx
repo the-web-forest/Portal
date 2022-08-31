19f877634c1e4d8f211af36ddcd118197616e3fe
barros <mdbf42@gmail.com>
2022-08-31 12:25:52 -0300

    Feature @ Header

--- src/sections/header/index.tsx
+++ src/sections/header/index.tsx
@@ -5,6 +5,8 @@ import Router from 'next/router';
 import pagePaths from '../../infra/core/pagePaths';
 import Vibrate from '../../utils/vibrate';
 import { useCart } from '../../providers/cart';
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
 
 interface HeaderProps {
   title?: string;
@@ -12,6 +14,7 @@ interface HeaderProps {
 
 const Header = ({ title }: HeaderProps) => {
   const cart = useCart();
+  const { signOut } = useContext(AuthContext);
 
   const goToShoppingCart = () => {
     Router.push(pagePaths.payment.shoppingCart);
@@ -41,15 +44,65 @@ const Header = ({ title }: HeaderProps) => {
     <header>
       <title>{renderTitle()}</title>
       <div className={styles.container}>
-        <div
-          id="logo"
-          className={styles.logo}
-          onClick={goToDashboard}
-          title={'Web Forest'}
-        >
-          <WebForestLogo />
+        <div className={styles.desktop}>
+          {' '}
+          <div
+            id="logo"
+            className={styles.logo}
+            onClick={goToDashboard}
+            title={'Web Forest'}
+          >
+            <WebForestLogo />
+          </div>
+          <div id="right-section" className={styles.rightSection}>
+            <div
+              id="cart-icon"
+              className={styles.shoppingCart}
+              onClick={goToShoppingCart}
+            >
+              <Image
+                width={30}
+                height={30}
+                src="/icons/shopping-cart-white.svg"
+              />
+              <div id="cart-number" className={styles.cartNumber}>
+                {cart.cartTotals.quantity}
+              </div>
+            </div>
+            <div
+              id="my-account"
+              onClick={goToMyAccount}
+              className={styles.myAccount}
+            >
+              <div id="my-photo" className={styles.myPhoto}>
+                <Image src={'/images/icons/user.svg'} width={40} height={40} />
+              </div>
+              <span id="my-account-text" className={styles.myAccountText}>
+                Minha Conta
+              </span>
+            </div>
+            <div
+              className={styles.logout}
+              onClick={() => signOut()}
+              title="Sair"
+            >
+              <Image src={'/icons/exit-icon.svg'} width={40} height={40} />
+            </div>
+          </div>
         </div>
-        <div id="right-section" className={styles.rightSection}>
+
+        <div className={styles.mobile}>
+          <div id="my-photo" className={styles.myPhoto}>
+            <Image src={'/images/icons/user.svg'} width={40} height={40} />
+          </div>
+          <div
+            id="logo"
+            className={styles.logo}
+            onClick={goToDashboard}
+            title={'Web Forest'}
+          >
+            <WebForestLogo />
+          </div>
           <div
             id="cart-icon"
             className={styles.shoppingCart}
@@ -58,23 +111,11 @@ const Header = ({ title }: HeaderProps) => {
             <Image
               width={30}
               height={30}
-              src="/images/icons/shopping-cart.svg"
+              src="/icons/shopping-cart-white.svg"
             />
             <div id="cart-number" className={styles.cartNumber}>
               {cart.cartTotals.quantity}
             </div>
-          </div>
-          <div
-            id="my-account"
-            onClick={goToMyAccount}
-            className={styles.myAccount}
-          >
-            <div id="my-photo" className={styles.myPhoto}>
-              <Image src={'/images/icons/user.svg'} width={40} height={40} />
-            </div>
-            <span id="my-account-text" className={styles.myAccountText}>
-              Minha Conta
-            </span>
           </div>
         </div>
       </div>
