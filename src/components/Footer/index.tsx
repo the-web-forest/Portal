71ae2b5eb317af832517564a2eba9add14972459
Valter Silva <svalter222@gmail.com>
2022-09-07 17:27:47 -0300

    feature - Componente Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,28 +4,28 @@ import {
   IoLogoInstagram,
   IoLogoLinkedin,
 } from 'react-icons/io5';
-import { WebForestLogo } from '../WebForestLogo';
+import Image from 'next/image';
 import styles from './styles.module.scss';
 
 export const Footer: FC = () => {
   return (
     <div id="footer" className={styles.container}>
-      <div className={styles.main}>
+      <div className={styles.logo}>
         <div className={styles.logoWrapper}>
-          <WebForestLogo />
-        </div>
-        <div className={styles.links}>
-          <a>Área da transparência</a>
-          <a>Política de privacidade</a>
+          <Image src="/images/logo-white.svg" width={100} height={40} />
         </div>
       </div>
-
+      <div className={styles.main}>
+        <div className={styles.links}>
+          <a>contato@webforest.eco</a>
+        </div>
+      </div>
       <div className={styles.social}>
         <span>Siga nossas Redes Sociais</span>
         <div className={styles.items}>
-          <IoLogoFacebook color="#4D4D4D" />
-          <IoLogoInstagram color="#4D4D4D" />
-          <IoLogoLinkedin color="#4D4D4D" />
+          <IoLogoFacebook color="#FFFFFF" />
+          <IoLogoInstagram color="#FFFFFF" />
+          <IoLogoLinkedin color="#FFFFFF" />
         </div>
       </div>
     </div>
