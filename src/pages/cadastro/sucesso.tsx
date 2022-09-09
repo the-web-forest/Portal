a1c93ad8e1b242c16100e20065d2f43d08e48820
barros <mdbf42@gmail.com>
2022-09-09 16:18:15 -0300

    Fix @ Names

--- src/pages/cadastro/sucesso.tsx
+++ src/pages/cadastro/sucesso.tsx
@@ -6,7 +6,7 @@ import styles from '../../styles/Signup.success.module.scss';
 
 const SignupSuccess: NextPage = () => (
   <div className={styles.container}>
-    <title>{`Cadastro com Sucesso! - ${Settings.APP_NAME}`}</title>
+    <title>{`Cadastro efetuado - ${Settings.APP_NAME}`}</title>
     <SignupHeader />
     <div className={styles.body}>
       <hr className={styles.line} />
