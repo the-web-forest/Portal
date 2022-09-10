256ae5071ffbb0d6856a2e73bbc7a60d3cb50b11
barros <mdbf42@gmail.com>
2022-09-10 18:37:10 -0300

    Fix @ GitIgnore

---
+++ src/infra/useCases/newPayment.usecase 2.ts
@@ -0,0 +1,38 @@
+import ApiURI from '../core/apiURI';
+import { HttpService } from '../services/HTTP.service';
+import { IHTTPService } from '../services/interfaces/IHTTPService';
+import INewPaymentRequest from '../dtos/NewPayment/INewPaymentRequest.dto';
+import { CartItem } from '../../utils/cart-utils';
+import INewPaymentResponse from '../dtos/NewPayment/INewPaymentResponse.dto';
+
+export default class NewPaymentUseCase {
+  private readonly httpService: IHTTPService;
+  constructor() {
+    this.httpService = new HttpService();
+  }
+
+  async run(items: CartItem[], cardToken: string): Promise<string> {
+    const requestBody: INewPaymentRequest = {
+      trees: [],
+      cardToken: cardToken,
+    };
+
+    items.forEach(item => {
+      requestBody.trees.push({
+        id: item.getId(),
+        quantity: item.getQuantity(),
+      });
+    });
+
+    const response = await this.httpService.post<
+      INewPaymentRequest,
+      INewPaymentResponse
+    >(ApiURI.Plant.index, requestBody);
+
+    if (!response.planted || !response.orderId) {
+      throw new Error('Error on Plant');
+    }
+
+    return response.orderId;
+  }
+}
---
+++ src/infra/useCases/sendEmailToValidateEmail.usecase 2.ts
@@ -0,0 +1,39 @@
+import ApiURI from '../core/apiURI';
+import ApiErrors from '../errors/ApiErrors';
+import SendEmailToValidateError from '../errors/SendEmailToValidateErrors';
+import { HttpService } from '../services/HTTP.service';
+import { IHTTPService } from '../services/interfaces/IHTTPService';
+
+type Response = {
+  send: boolean;
+};
+
+type DataBody = {
+  email: string;
+};
+
+export default class SendEmailToValidateEmailUseCase {
+  private readonly httpService: IHTTPService;
+
+  constructor() {
+    this.httpService = new HttpService();
+  }
+
+  async run(email: string): Promise<boolean> {
+    try {
+      const data: DataBody = { email };
+      return (
+        await this.httpService.post<DataBody, Response>(
+          `${ApiURI.User.sendValidationEmail}`,
+          data,
+        )
+      ).send;
+    } catch (error: any) {
+      const data = error.response.data;
+      if (data == undefined) {
+        console.error('Unknow Error:', error);
+      }
+      throw new ApiErrors(SendEmailToValidateError).getError(data.Code);
+    }
+  }
+}
